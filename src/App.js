fec26a61c4445babcd34bcc0c7f26b58d45f8b1d
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2021-11-06 15:35:36 +0500

    Finish All

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import content from "./asset/content";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
+  const [lang, setLang] = useState("uz");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="container-fluid bg-info vh-100 d-flex align-items-center justify-content-center">
+        <form
+          className="shadow-lg p-3 mt-4 bg-body rounded"
+          style={{ width: "450px" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <select
+            className="form-select ms-auto"
+            style={{ width: "120px" }}
+            onChange={(evt) => setLang(evt.target.value)}
+            defaultValue={lang}
+          >
+            <option value="uz">Uzbek</option>
+            <option value="en">English</option>
+            <option value="ru">Russian</option>
+          </select>
+          <label htmlFor="email">
+            <p className="mb-2">{content[lang].emailLabelStart}</p>
+            <input
+              id="email"
+              className="form-control"
+              style={{ width: "400px" }}
+              type="email"
+              placeholder={content[lang].emailPlaceholder}
+              autoFocus
+              required
+            />
+            <p className="text-warning">{content[lang].emailLabelEnd}</p>
+          </label>
+          <label htmlFor="parol" className="mt-2">
+            <p className="mb-2">{content[lang].parolLabelStart}</p>
+            <input
+              id="parol"
+              className="form-control"
+              style={{ width: "400px" }}
+              type="password"
+              placeholder={content[lang].parolPlaceholder}
+              required
+            />
+            <p className="text-warning">{content[lang].parolLabelEnd}</p>
+          </label>
+          <label>
+            <input className="form-check-input" type="checkbox" />
+            <span className="ms-2">{content[lang].remember}</span>
+          </label>
+          <div>
+            <button className="btn btn-primary mt-3 w-50 text-center">
+              {content[lang].send}
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
 
---
+++ src/asset/content.js
@@ -0,0 +1,34 @@
+const content = {
+  uz: {
+    emailLabelStart: "Email manzil:",
+    emailPlaceholder: "Emailingizni kiriting...",
+    emailLabelEnd: "Emailingiz tarqalmaydi!",
+    parolLabelStart: "Parolingiz:",
+    parolPlaceholder: "Parolingizni kiriting...",
+    parolLabelEnd: "Parolingiz tarqalmaydi!",
+    remember: "Eslab qolish!",
+    send: "Yuborish!",
+  },
+  en: {
+    emailLabelStart: "Email:",
+    emailPlaceholder: "Input your email:",
+    emailLabelEnd: "we won`t send your email to anyone!",
+    parolLabelStart: "Parol:",
+    parolPlaceholder: "Input your parol...",
+    parolLabelEnd: "we won`t send your parol to anyone!",
+    remember: "Remember!",
+    send: "Send!",
+  },
+  ru: {
+    emailLabelStart: "Эл. почта:",
+    emailPlaceholder: "Введите свой адрес электронной почты...",
+    emailLabelEnd: "ваша электронная почта не будет распространяться!",
+    parolLabelStart: "Пароль:",
+    parolPlaceholder: "Введите пароль...",
+    parolLabelEnd: "Ваш пароль не будет выдан!",
+    remember: "Вспомнить!",
+    send: "Отправить!",
+  },
+};
+
+export default content;
